0fd27825dab95524fa0be4280d9f7ee9aad2f305
Will Patton <willapatton@icloud.com>
2026-06-13 15:45:32 -0400

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.min_stack and val < self.min_stack[-1]:
+            self.min_stack.append(val)
+        elif self.min_stack:
+            self.min_stack.append(self.min_stack[-1])
+        else:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
